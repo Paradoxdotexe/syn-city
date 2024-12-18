fcf25f6a16ca0f96364ecc4472f159ebddb5ddf7
Nathan Klingensmith <paradoxpyt@gmail.com>
2024-12-17 20:51:33 -0500

    Update icons

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Card.css";
-import { ReactComponent as HeartIcon } from "./icons/Heart.svg";
 import { ReactComponent as BrainIcon } from "./icons/Brain.svg";
-import { ReactComponent as BoltIcon } from "./icons/Bolt.svg";
+import { ReactComponent as HeartIcon } from "./icons/Heart.svg";
+import { ReactComponent as ShieldIcon } from "./icons/Shield.svg";
+import { ReactComponent as EyeIcon } from "./icons/Eye.svg";
+import { ReactComponent as FistIcon } from "./icons/Fist.svg";
+import { ReactComponent as CrosshairIcon } from "./icons/Crosshair.svg";
 import { ReactComponent as ImageBorder } from "./icons/ImageBorder.svg";
 import { ReactComponent as ImageGrid } from "./icons/ImageGrid.svg";
 import { parse } from "marked";
@@ -14,10 +17,8 @@ type CardAttributeKey =
   | "precision"
   | "protocol"
   | "persona"
-  | "preservation"
+  | "plating"
   | "perception";
-
-type CardAttributeType = "kinetic" | "neural" | "core";
 
 export type SynCityCard = {
   actionType: CardActionType;
@@ -28,29 +29,30 @@ export type SynCityCard = {
   description: string;
 };
 
-const ATTRIBUTE_TYPES: { [key in CardAttributeKey]: CardAttributeType } = {
-  power: "kinetic",
-  precision: "kinetic",
-  protocol: "neural",
-  persona: "neural",
-  preservation: "core",
-  perception: "core",
+const KINETIC_COLOR = "#CA84FF";
+const NEURAL_COLOR = "#D4FF6F";
+const CORE_COLOR = "#3EFDD7";
+
+const ATTRIBUTE_COLORS: { [key in CardAttributeKey]: string } = {
+  power: KINETIC_COLOR,
+  precision: KINETIC_COLOR,
+  protocol: NEURAL_COLOR,
+  persona: NEURAL_COLOR,
+  plating: CORE_COLOR,
+  perception: CORE_COLOR,
 };
 
-const ATTRIBUTE_TYPE_COLORS: { [key in CardAttributeType]: string } = {
-  kinetic: "#CA84FF",
-  neural: "#D4FF6F",
-  core: "#3EFDD7",
-};
-
-const ATTRIBUTE_TYPE_ICONS: {
-  [key in CardAttributeType]: React.FunctionComponent<
+const ATTRIBUTE_ICONS: {
+  [key in CardAttributeKey]: React.FunctionComponent<
     React.SVGAttributes<SVGElement>
   >;
 } = {
-  kinetic: BoltIcon,
-  neural: BrainIcon,
-  core: HeartIcon,
+  power: FistIcon,
+  precision: CrosshairIcon,
+  protocol: BrainIcon,
+  persona: HeartIcon,
+  plating: ShieldIcon,
+  perception: EyeIcon,
 };
 
 type CardProps = {
@@ -59,52 +61,32 @@ type CardProps = {
 };
 
 export const Card: React.FC<CardProps> = (props) => {
-  const attributeType = ATTRIBUTE_TYPES[props.card.attributeKey];
-  const attributeTypeColor = ATTRIBUTE_TYPE_COLORS[attributeType];
-  const AttributeTypeIcon = ATTRIBUTE_TYPE_ICONS[attributeType];
+  const color = ATTRIBUTE_COLORS[props.card.attributeKey];
+  const Icon = ATTRIBUTE_ICONS[props.card.attributeKey];
 
   return (
     <div
       className="card"
       style={{
-        color: attributeTypeColor,
+        color: color,
         ...props.style,
       }}
     >
       <div className="card__bg bg--image" />
-      <div
-        className="card__bg bg--overlay"
-        style={{ background: attributeTypeColor }}
-      />
+      <div className="card__bg bg--overlay" style={{ background: color }} />
 
       <div className="card__header">
-        <div style={{ display: "flex", alignItems: "center", gap: 8 }}>
-          <div
-            className="header__attributeType"
-            style={{ background: attributeTypeColor }}
-          >
-            <AttributeTypeIcon />
-          </div>
-          <div
-            className="header__actionCost"
-            style={{ color: attributeTypeColor }}
-          >
-            {props.card.actionCost}
-          </div>
+        <div className="header__actionCost" style={{ background: color }}>
+          {props.card.actionCost}
         </div>
 
         <div className="header__attribute">
+          <Icon />
           <div className="attribute__name">{props.card.attributeKey}</div>
           {props.card.attributeCost > 8 && (
-            <div
-              className="attribute__cost"
-              style={{ background: attributeTypeColor }}
-            />
+            <div className="attribute__cost" style={{ background: color }} />
           )}
-          <div
-            className="attribute__cost"
-            style={{ background: attributeTypeColor }}
-          >
+          <div className="attribute__cost" style={{ background: color }}>
             {[...new Array(8)].map((_, i) => {
               const bit = props.card.attributeCost % 8;
               return <div>{i < bit || bit === 0 ? "1" : "0"}</div>;
@@ -114,7 +96,7 @@ export const Card: React.FC<CardProps> = (props) => {
       </div>
 
       <div className="card__image">
-        <ImageBorder style={{ fill: attributeTypeColor }} />
+        <ImageBorder style={{ fill: color }} />
         <ImageGrid className="image__grid" />
       </div>
 
@@ -136,7 +118,7 @@ export const Card: React.FC<CardProps> = (props) => {
         }}
       />
 
-      <div className="card__footer" style={{ background: attributeTypeColor }}>
+      <div className="card__footer" style={{ background: color }}>
         <div>SynCityGame.com</div>
         <div>Copyright © 2025 Syn City</div>
       </div>
